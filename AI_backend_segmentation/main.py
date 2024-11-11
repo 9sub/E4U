0a3ac492a9174470a44533e779f1713ebf5df28a
9sub <guseob0701@naver.com>
2024-11-11 17:36:33 +0900

    feat: add baseline fastapi

--- AI_backend_segmentation/main.py
+++ AI_backend_segmentation/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
+@app.post("/segmentation/image/")
+def send_segmentation_image():
+    result_image_path = "/workspace/AI/CV/teeth_segmentation/output/teethnum.png"
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+    return FileResponse(result_image_path, media_type="image/png", filename="segmentation_result.png")
